1524e0f9b82ea994597c02dc9145759425deeb94
aloha zeng <myforestory@hotmail.com.tw>
2018-09-25 12:53:53 +0800

    version2.0 remove 400 exception

--- src/main/java/tw/idv/aloha/lineBot/controller/LineBotRSController.java
+++ src/main/java/tw/idv/aloha/lineBot/controller/LineBotRSController.java
@@ -132,32 +132,32 @@ public class LineBotRSController {
 	}
 	
 	protected void sendResponseError(String replyToken, String message) {
-		System.out.println(message);
-		
-		try {
-			
-			message = "{\"replyToken\":\"" + replyToken + "\",\"messages\":" + message + "}"; // 回傳的json格式訊息
-			// System.out.println(System.getProperty("line.separator") +
-			// message);
-			URL myurl = new URL("https://api.line.me/v2/bot/message/reply"); // 回傳的網址
-			HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection(); // 使用HttpsURLConnection建立https連線
-			con.setRequestMethod("POST");// 設定post方法
-			con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 設定Content-Type為json
-			con.setRequestProperty("Authorization", "Bearer " + this.accessToken); // 設定Authorization
-			con.setDoOutput(true);
-			con.setDoInput(true);
-			DataOutputStream output = new DataOutputStream(con.getOutputStream()); // 開啟HttpsURLConnection的連線
-			
-			output.write(message.getBytes(Charset.forName("utf-8"))); // 回傳訊息編碼為utf-8
-			output.close();
-			System.out.println("Resp Code:" + con.getResponseCode() + "; Resp Message:" + con.getResponseMessage()); // 顯示回傳的結果，若code為200代表回傳成功
-		} catch (MalformedURLException e) {
-			System.out.println("Message: " + e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Message: " + e.getMessage());
-			e.printStackTrace();
-		}
+//		System.out.println(message);
+//		
+//		try {
+//			
+//			message = "{\"replyToken\":\"" + replyToken + "\",\"messages\":" + message + "}"; // 回傳的json格式訊息
+//			// System.out.println(System.getProperty("line.separator") +
+//			// message);
+//			URL myurl = new URL("https://api.line.me/v2/bot/message/reply"); // 回傳的網址
+//			HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection(); // 使用HttpsURLConnection建立https連線
+//			con.setRequestMethod("POST");// 設定post方法
+//			con.setRequestProperty("Content-Type", "application/json; charset=utf-8"); // 設定Content-Type為json
+//			con.setRequestProperty("Authorization", "Bearer " + this.accessToken); // 設定Authorization
+//			con.setDoOutput(true);
+//			con.setDoInput(true);
+//			DataOutputStream output = new DataOutputStream(con.getOutputStream()); // 開啟HttpsURLConnection的連線
+//			
+//			output.write(message.getBytes(Charset.forName("utf-8"))); // 回傳訊息編碼為utf-8
+//			output.close();
+//			System.out.println("Resp Code:" + con.getResponseCode() + "; Resp Message:" + con.getResponseMessage()); // 顯示回傳的結果，若code為200代表回傳成功
+//		} catch (MalformedURLException e) {
+//			System.out.println("Message: " + e.getMessage());
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			System.out.println("Message: " + e.getMessage());
+//			e.printStackTrace();
+//		}
 	}
 	
 	
